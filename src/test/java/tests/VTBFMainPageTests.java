1da8e54d8ca004cffb5ee8c6c7f594ee272c07e5
EkaterinaMelnikova <katkimo@gmail.com>
2025-12-11 01:26:38 +0300

    first commit

---
+++ src/test/java/tests/VTBFMainPageTests.java
@@ -0,0 +1,60 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class VTBFMainPageTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://www.vtbf.ru";
+    }
+    @Test
+    void OpenAboutCompanyTest() {
+
+        open("/");
+
+        $(byText("О компании")).click();
+        $(".b-section").shouldHave(text("О компании"),
+                text("Брошюра о ВТБ Факторинг"));
+        sleep(1000);
+    }
+
+
+    @Test
+    void OpenKBTest() {
+
+        open("/");
+
+        $(byText("Крупному бизнесу")).click();
+        $(byText("Финансирование закупок")).click();
+        sleep(1000);
+    }
+
+    @Test
+    void OpenСasesTest() {
+
+        open("/");
+
+        $(byText("Смотреть все")).click();
+        $(".b-section").shouldHave(text("Кейсы"));
+        sleep(1000);
+    }
+
+    @Test
+    void OpenСareerFooterTest() {
+
+        open("/");
+
+        $(byText("Карьера")).click();
+        $(".b-section").shouldHave(text("Работа у нас"));
+        sleep(1000);
+    }
+
+}
